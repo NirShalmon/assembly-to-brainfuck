1da004b8a88615c6171e8bcea29ae4dd375400a5
NirShalmon <nir.shalmon00@gmail.com>
2020-05-18 12:32:25 +0300

    test add

--- tests/AsmTests.py
+++ tests/AsmTests.py
@@ -103,6 +103,81 @@ class AsmTests(unittest.TestCase):
         self.assertTrue(debugger.execution_completed)
         self.assertEqual(debugger.get_vmc_value(controller.offset_reg[0], as_signed_num=True), -123456789)
 
+    def test_add_two_immediates(self):
+        code = """4 256 14 5 True
+                  add r0 999 -99"""
+        controller = VMCController(4, 256, 14, 5)
+        debugger = Debugger(controller, compile_code(code))
+        debugger.exec_commands(10000000)
+        self.assertTrue(debugger.execution_completed)
+        self.assertEqual(debugger.get_vmc_value(controller.offset_reg[0], as_signed_num=True), 900)
+
+    def test_add_immediate_register(self):
+        code = """4 256 14 5 True
+                  mov r1 1
+                  add r0 999 r1"""
+        controller = VMCController(4, 256, 14, 5)
+        debugger = Debugger(controller, compile_code(code))
+        debugger.exec_commands(10000000)
+        self.assertTrue(debugger.execution_completed)
+        self.assertEqual(debugger.get_vmc_value(controller.offset_reg[0], as_signed_num=True), 1000)
+        self.assertEqual(debugger.get_vmc_value(controller.offset_reg[1], as_signed_num=True), 1)
+
+    def test_add_register_register(self):
+        code = """4 256 14 5 True
+                  mov r1 1
+                  mov r2 3
+                  add r0 r2 r1"""
+        controller = VMCController(4, 256, 14, 5)
+        debugger = Debugger(controller, compile_code(code))
+        debugger.exec_commands(10000000)
+        self.assertTrue(debugger.execution_completed)
+        self.assertEqual(debugger.get_vmc_value(controller.offset_reg[0], as_signed_num=True), 4)
+        self.assertEqual(debugger.get_vmc_value(controller.offset_reg[2], as_signed_num=True), 3)
+        self.assertEqual(debugger.get_vmc_value(controller.offset_reg[1], as_signed_num=True), 1)
+
+    def test_add_3_times_same_register(self):
+        code = """4 256 14 5 True
+                  mov r0 5
+                  add r0 r0 r0"""
+        controller = VMCController(4, 256, 14, 5)
+        debugger = Debugger(controller, compile_code(code))
+        debugger.exec_commands(10000000)
+        self.assertTrue(debugger.execution_completed)
+        self.assertEqual(debugger.get_vmc_value(controller.offset_reg[0], as_signed_num=True), 10)
+
+    def test_increment_register_left(self):
+        code = """4 256 14 5 True
+                  mov r0 5
+                  add r0 7 r0"""
+        controller = VMCController(4, 256, 14, 5)
+        debugger = Debugger(controller, compile_code(code))
+        debugger.exec_commands(10000000)
+        self.assertTrue(debugger.execution_completed)
+        self.assertEqual(debugger.get_vmc_value(controller.offset_reg[0], as_signed_num=True), 12)
+
+    def test_increment_register_right(self):
+        code = """4 256 14 5 True
+                  mov r0 5
+                  add r0 r0 7"""
+        controller = VMCController(4, 256, 14, 5)
+        debugger = Debugger(controller, compile_code(code))
+        debugger.exec_commands(10000000)
+        self.assertTrue(debugger.execution_completed)
+        self.assertEqual(debugger.get_vmc_value(controller.offset_reg[0], as_signed_num=True), 12)
+
+    def test_increment_register_with_register(self):
+        code = """4 256 14 5 True
+                  mov r0 5
+                  mov r2 12340
+                  add r0 r0 r2"""
+        controller = VMCController(4, 256, 14, 5)
+        debugger = Debugger(controller, compile_code(code))
+        debugger.exec_commands(10000000)
+        self.assertTrue(debugger.execution_completed)
+        self.assertEqual(debugger.get_vmc_value(controller.offset_reg[0], as_signed_num=True), 12345)
+        self.assertEqual(debugger.get_vmc_value(controller.offset_reg[2], as_signed_num=True), 12340)
+
 
 if __name__ == '__main__':
     unittest.main()
