fdd44b2d5f6e23d8df351a93214c63650f10f16c
NirShalmon <nir.shalmon00@gmail.com>
2020-05-18 12:59:36 +0300

    Some refactoring and the start of less implementation

--- Command.py
+++ Command.py
@@ -9,7 +9,7 @@ def put_value_in_temp(controller: VMCController, operand: Operand):
     Otherwise, allocate a temp register, copy operand's value to it and return
     Also return the code created
     """
-    if operand.type == OperandType.register:
+    if operand.is_register():
         return operand.value, ''
     temp = controller.temp_allocator.alloc_temp()
     return temp, controller.set_num(temp, operand.value)
@@ -21,7 +21,7 @@ def force_value_in_temp(controller: VMCController, operand: Operand):
     Return the temp and the code
     """
     temp = controller.temp_allocator.alloc_temp()
-    if operand.type == OperandType.immediate:
+    if operand.is_immediate():
         code = controller.set_num(temp, operand.value)
     else:
         code = controller.copy_num(operand.value, temp)
@@ -32,7 +32,7 @@ def free_possible_temp(controller: VMCController, operand: Operand, register):
     """
     Free the register returned by put_value_in_temp, if it is temp
     """
-    if operand.type == OperandType.immediate:
+    if operand.is_immediate():
         controller.temp_allocator.free(register)
 
 
@@ -60,7 +60,7 @@ class Command:
             CommandType.neg: self.compile_neg,
             CommandType.add: self.compile_add,
             CommandType.sub: self.compile_sub,
-            # CommandType.less: self.compile_less,
+            CommandType.less: self.compile_less,
             # CommandType.eq: self.compile_eq,
             # CommandType.logic_not: self.compile_logic_not,
             # CommandType.logic_and: self.compile_logic_and,
@@ -80,20 +80,20 @@ class Command:
 
     def compile_mov(self, controller: VMCController, label_to_basic_block):
         assert len(self.operands) == 2
-        assert self.operands[0].type == OperandType.register
-        assert self.operands[1].type != OperandType.label
+        assert self.operands[0].is_register()
+        assert not self.operands[1].is_label()
         if self.operands[0] == self.operands[1]:
             return ''
-        if self.operands[1].type == OperandType.immediate:
+        if self.operands[1].is_immediate():
             return controller.set_num(self.operands[0].value, self.operands[1].value)
         return controller.copy_num(self.operands[1].value, self.operands[0].value)
 
     def compile_store(self, controller: VMCController, label_to_basic_block):
         assert len(self.operands) == 2
-        assert self.operands[0].type != OperandType.label
-        assert self.operands[1].type != OperandType.label
+        assert not self.operands[0].is_label()
+        assert not self.operands[1].is_label()
         addr_offset, addr_code = put_value_in_temp(controller, self.operands[0])
-        if self.operands[1].type == OperandType.immediate:
+        if self.operands[1].is_immediate():
             code = addr_code + controller.store_memory(addr_offset, self.operands[1].value, value_is_immediate=True)
         else:
             code = addr_code + controller.store_memory(addr_offset, self.operands[1].value)
@@ -102,8 +102,8 @@ class Command:
 
     def compile_load(self, controller: VMCController, label_to_basic_block):
         assert len(self.operands) == 2
-        assert self.operands[0].type == OperandType.register
-        assert self.operands[1].type != OperandType.label
+        assert self.operands[0].is_register()
+        assert not self.operands[1].is_label()
         addr_offset, addr_code = put_value_in_temp(controller, self.operands[1])
         code = addr_code + controller.load_memory(addr_offset, self.operands[0].value)
         free_possible_temp(controller, self.operands[1], addr_offset)
@@ -111,19 +111,19 @@ class Command:
 
     def compile_neg(self, controller: VMCController, label_to_basic_block):
         assert len(self.operands) == 1
-        assert self.operands[0].type == OperandType.register
+        assert self.operands[0].is_register()
         return controller.negate_num(self.operands[0].value)
 
     def compile_add(self, controller: VMCController, label_to_basic_block):
         assert len(self.operands) == 3
-        assert self.operands[0].type == OperandType.register
-        assert self.operands[1].type != OperandType.label
-        assert self.operands[2].type != OperandType.label
-        if self.operands[1].type == OperandType.immediate and self.operands[2].type == OperandType.immediate:
+        assert self.operands[0].is_register()
+        assert not self.operands[1].is_label()
+        assert not self.operands[2].is_label()
+        if self.operands[1].is_immediate() and self.operands[2].is_immediate():
             return controller.set_num(self.operands[0].value, self.operands[1].value + self.operands[2].value)
-        if self.operands[1].value == self.operands[0].value and self.operands[2].type == OperandType.immediate:
+        if self.operands[1].value == self.operands[0].value and self.operands[2].is_immediate():
             return controller.increment_num(self.operands[0].value, self.operands[2].value)
-        if self.operands[2].value == self.operands[0].value and self.operands[1].type == OperandType.immediate:
+        if self.operands[2].value == self.operands[0].value and self.operands[1].is_immediate():
             return controller.increment_num(self.operands[0].value, self.operands[1].value)
         lhs_temp, lhs_code = force_value_in_temp(controller, self.operands[1])
         rhs_temp, rhs_code = force_value_in_temp(controller, self.operands[2])
@@ -135,12 +135,12 @@ class Command:
 
     def compile_sub(self, controller: VMCController, label_to_basic_block):
         assert len(self.operands) == 3
-        assert self.operands[0].type == OperandType.register
-        assert self.operands[1].type != OperandType.label
-        assert self.operands[2].type != OperandType.label
-        if self.operands[1].type == OperandType.immediate and self.operands[2].type == OperandType.immediate:
+        assert self.operands[0].is_register()
+        assert not self.operands[1].is_label()
+        assert not self.operands[2].is_label()
+        if self.operands[1].is_immediate() and self.operands[2].is_immediate():
             return controller.set_num(self.operands[0].value, self.operands[1].value - self.operands[2].value)
-        if self.operands[1].value == self.operands[0].value and self.operands[2].type == OperandType.immediate:
+        if self.operands[1].value == self.operands[0].value and self.operands[2].is_immediate():
             return controller.increment_num(self.operands[0].value, -self.operands[2].value)
         lhs_temp, lhs_code = force_value_in_temp(controller, self.operands[1])
         rhs_temp, rhs_code = force_value_in_temp(controller, self.operands[2])
@@ -149,3 +149,13 @@ class Command:
         add_code = controller.sub_num(self.operands[0].value, rhs_temp)
         controller.temp_allocator.free(rhs_temp)
         return lhs_code + rhs_code + first_copy + add_code
+
+    def compile_less(self, controller: VMCController, label_to_basic_block):
+        assert len(self.operands) == 3
+        assert self.operands[0].type == OperandType.register
+        assert self.operands[1].type != OperandType.label
+        assert self.operands[2].type != OperandType.label
+        code = [controller.clear_num(self.operands[0].value)]
+        result_byte = self.operands[0].value + controller.num_size - 1
+        if self.operands[1].type == OperandType.immediate and self.operands[2].type == OperandType.immediate:
+            pass
